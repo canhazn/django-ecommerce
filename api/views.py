6ea54215ba432d0c02c842046d20e3bb277575e2
tuan anh <canhazn@gmail.com>
2020-05-04 10:23:07 +0700

    new .venv

--- api/views.py
+++ api/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Product
+from .serializers import ProductSerializer
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 # Create your views here.
+
+@api_view(['GET'])
+def ApiOverview(request):
+    return Response({"mess": "hello"})
+
+@api_view(['GET'])
+def ProductList(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    print(serializer.data)
+    return Response(serializer.data)
--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'store'
+    'rest_framework',
+    'api'
+    # 'store'
 ]
 
 MIDDLEWARE = [
